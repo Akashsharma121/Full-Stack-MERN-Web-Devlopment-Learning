f57df077234c782439f9daed8bb9b6e79310f05b
Akash_sharma <akashakash87928@gmail.com>
2024-09-23 18:15:56 +0530

    I Learn Coding

---
+++ Day 25/Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,19 @@
+alert("I am Akash")
+
+console.log("code is running...")
+console.log("code is also running...")
+console.log("code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var istrue = confirm("Are you sure you want to leave is page and blast your computer")
+if(istrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+console.log("Your number is " + a)
+
+document.title = "hey i am good"
+
+// document.body.style.backgroundColor = "red"
