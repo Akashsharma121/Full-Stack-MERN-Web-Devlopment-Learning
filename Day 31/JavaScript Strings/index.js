65c1df87b415055604fdbe7828d68bd9af9be7b0
Akash_sharma <akashakash87928@gmail.com>
2024-09-30 19:00:04 +0530

    I Learn Coding

---
+++ Day 31/JavaScript Strings/index.js
@@ -0,0 +1,27 @@
+console.log("This is a string tutorial")
+let a = "Akash";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let name = "Akash"
+let friend = "Harry"
+console.log("His name is " + name +" and his friend name is " + friend)
+console.log(`His name is ${name} and his friend name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh", "66"))
+console.log(b.concat(a, "aishwariya", "rohan", "riya"))
+
+console.log(b)
