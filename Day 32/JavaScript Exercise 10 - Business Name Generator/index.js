cafb254689abe87d95f5c5c97d55431e478d9d55
Akash_sharma <akashakash87928@gmail.com>
2024-10-01 19:30:03 +0530

    I Learn Coding

---
+++ Day 32/JavaScript Exercise 10 - Business Name Generator/index.js
@@ -0,0 +1,19 @@
+// Creat a busness name generator by combining list of adjective and shop name and another words
+
+/*
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Words:
+Bros Limited
+Hub
+
+*/
