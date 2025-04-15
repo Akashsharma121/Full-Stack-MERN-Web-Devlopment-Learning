c8c01d8a8168e0e143aba04d9e3ce10548e94366
Akash_sharma <akashakash87928@gmail.com>
2025-04-16 02:26:53 +0530

    I Learn Coding

---
+++ Day 84/Script, Link & Image components in Next.js/my-app/app/contact/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      This is Contact
+    </div>
+  )
+}
+
+export default contact
+
+export const metadata = {
+    title: "Contact Facebook - Contect with the world",
+    description: "This is contact Facebook and we are connect with the world using Facebook",
+  };
