de99b90c16fb9fedd918c9f152b61bcb6ed072ca
Akash_sharma <akashakash87928@gmail.com>
2025-08-02 00:13:38 +0530

    I Learn Coding

---
+++ Day 97/[Project] Let's Build a Url Shortener in Next.js 15/bitlinks/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Bitlinks - Your URL Shortener",
+  description: "Bitlinks is a URL shortener that allows you to create short links for your long URLs.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
